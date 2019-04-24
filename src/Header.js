440fab7e01611bdcfd8b61542569e126d4cbef66
Filipe <filsjust97@gmail.com>
2019-04-24 19:41:02 +0100

    Header

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import './Header.css';
+import Background from './img/bg-masthead.jpg';
+
+const myStyles = {
+    backgroundImage: `url(${Background})`,
+    height: '50vh',
+    backgroundSize: 'cover',
+}
+
+class Header extends Component {    
+    render() {
+        return (
+            <header style={myStyles}>
+                <h1>{this.props.title}</h1>
+                <p>A Portfolio</p>
+                <a href="#button">{this.props.button}</a>
+            </header>
+        );
+    }
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+/* import App from './App'; */
+import Navigation from './Navigation';
+import Header from './Header';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navigation logoTitle="Filipe's Portfolio" />
+                <Header title="Stylish Portfolio" button="Find Out More"/>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
